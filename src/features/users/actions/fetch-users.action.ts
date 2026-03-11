11729eb5eada0ccc9c57b44b9a9b93a691ac25b9
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-03-10 20:48:10 -0500

    fix: change bitacoraApi return value

--- src/features/users/actions/fetch-users.action.ts
+++ src/features/users/actions/fetch-users.action.ts
@@ -1,13 +1,8 @@
 import { bitacoraApi } from "../../../api/bitacora.api"
 import { User } from "../interfaces/user.interface"
-import { UsersApiResponse } from "../interfaces/user.response"
 
 export const fetchUsers = async (): Promise<User[]> => {
-  const response = await bitacoraApi<UsersApiResponse>('/user',{
-      headers: {
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMGRmNWE5LWQ0ODAtNDRkYy05ZWFmLTYwMDc3Mjc3N2NlYSIsImVtYWlsIjoiZWR3YXJkLmNydXpjcnV6MjcwNDE5OTZAZ21haWwuY29tIiwiaWF0IjoxNzcxOTA3NTg4LCJleHAiOjE3NzE5MTExODh9.lRGRD9fe8JtXvSconZ2cMhK1SwwwS4XEaCYudw9aQEs'
-      }
-    })
+  const response = await bitacoraApi<User[]>('/user')
 
-  return response.data.data;
+  return response.data;
 }
