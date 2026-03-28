32871bb41ea90bbdd10ff68756f3558d7fb4b247
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-03-28 14:27:56 -0500

    feat(login): adjust left/right spacing for inputs and button and add organization logo to LoginPage

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import styles from "../../../styles/features/auth/pages/AuthPage.module.scss";
 import Spinner from '../../../shared/components/Spinner';
 import SharedError from '../../../shared/components/SharedError';
 import { useLogin } from '../hooks/useLogin';
+import plantIcon from '../../../assets/plant-logo.png'
 
 
 const LoginPage = () => {
@@ -10,6 +11,7 @@ const LoginPage = () => {
     return (
         <div className={styles.container}>
             <div className={styles.card}>
+                <img src={plantIcon} alt="Company Logo" className={styles.logo}/>
                 <h3>Sign in</h3>
                 {error && (<SharedError 
                     variant='inline'
@@ -17,7 +19,7 @@ const LoginPage = () => {
                     //onRetry={handleLogin}
                 />)}
                 <p>
-                    Don't have an account?{" "}
+                    Don't have an account yet?{" "}
                     <Link to="/register">Sign up Here</Link>
                 </p>
                 <form className={styles.form}
@@ -30,34 +32,30 @@ const LoginPage = () => {
                         <input
                             className={styles.input}
                             type="email"
-                            placeholder='Correo electrónico'
+                            placeholder='Email'
                             value={email}
                             onChange={(e)=> setEmail(e.target.value)}
                             required
                         />
-                        {validationErrors.email && (
-                            <p className={styles.error}>
-                                {validationErrors.email}
-                            </p>
-                        )}
+                        <p className={styles.error}>
+                            {validationErrors.email || ''}
+                        </p>
                     </div>
                     <div className={styles.formGroup}>
                         <input
                             className={styles.input}
                             type="password"
-                            placeholder='Contraseña'
+                            placeholder='Password'
                             value={password}
                             onChange={(e)=> setPassword(e.target.value)}
                             required
                         />
-                        {validationErrors.password && (
-                            <p className={styles.error}>
-                                {validationErrors.password}
-                            </p>
-                        )}
+                        <p className={styles.error}>
+                            {validationErrors.password || ''}
+                        </p>
                     </div>
                     <button type="submit" className={styles.button} disabled={loading} onClick={handleLogin}>
-                        {loading ? <Spinner />: 'Iniciar Sesión'}
+                        {loading ? <Spinner />: 'Sign in'}
                     </button>
                 </form>
             </div>
